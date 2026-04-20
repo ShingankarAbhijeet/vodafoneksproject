974590bd92a8c51e4f86d7c096a1e9153595eeaa
devops <devopsprod11>
2026-04-20 08:05:04 +0530

    reverted

--- ecr.tf
+++ ecr.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_ecr_repository" "examplereg" {
   for_each = toset(var.env)
   name = "${var.Project}/${each.value}"
@@ -31,3 +32,4 @@ resource "aws_ecr_lifecycle_policy" "example" {
     ]
   })
 }
+*/
