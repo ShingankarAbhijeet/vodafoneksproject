bca5c50cb05bd3c6bb9a4e028b05f9b555a81905
devops <devopsprod11>
2026-05-01 12:29:14 +0530

    build

--- infra/modules/eks/eks-access.tf
+++ infra/modules/eks/eks-access.tf
@@ -1,4 +1,4 @@
-/*
+
 resource "aws_eks_access_entry" "github" {
   cluster_name  = aws_eks_cluster.example.name
   principal_arn = "arn:aws:iam::314133462348:role/github-actions-prod"
@@ -8,11 +8,10 @@ resource "aws_eks_access_entry" "github" {
 resource "aws_eks_access_policy_association" "github_admin" {
   cluster_name  = aws_eks_cluster.example.name
   principal_arn = "arn:aws:iam::314133462348:role/github-actions-prod"
-
+ 
   policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
   access_scope {
     type = "cluster"
   }
 }
-*/
