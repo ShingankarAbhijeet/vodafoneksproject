8f1b5abb9d2edfb9b2f0d6d658b790a708008991
devops <devopsprod11>
2026-05-01 23:08:00 +0530

    destroy

--- infra/modules/eks/eks-access.tf
+++ infra/modules/eks/eks-access.tf
@@ -1,4 +1,4 @@
-
+/*
 resource "aws_eks_access_entry" "github" {
   cluster_name  = aws_eks_cluster.example.name
   principal_arn = "arn:aws:iam::314133462348:role/github-actions-prod"
@@ -15,3 +15,6 @@ resource "aws_eks_access_policy_association" "github_admin" {
     type = "cluster"
   }
 }
+
+
+*/
