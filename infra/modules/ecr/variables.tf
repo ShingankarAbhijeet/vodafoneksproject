08ad4a0810325139f29f7d75cc4037beababbed9
devops <devopsprod11>
2026-05-10 11:08:09 +0530

    updated ECR structure

--- infra/modules/ecr/variables.tf
+++ infra/modules/ecr/variables.tf
@@ -5,7 +5,7 @@ variable "subnet" {
 
 variable "env" {
   type = list(string)
-  default = ["dev", "qa", "staging", "prod"]  #"app-repo"]
+  default = ["dev", "prod", "addons"]  #"app-repo"]
   
 }
 variable "Project" {
