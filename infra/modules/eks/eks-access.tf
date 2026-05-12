728404128cf6a8a96874e316e6b9412b63255cdb
devops <devopsprod11>
2026-05-12 07:49:43 +0530

    build

--- infra/modules/eks/eks-access.tf
+++ infra/modules/eks/eks-access.tf
@@ -1,4 +1,4 @@
-/*
+
 resource "aws_eks_access_entry" "github" {
   cluster_name  = aws_eks_cluster.example.name
   principal_arn = "arn:aws:iam::314133462348:role/github-actions-prod"
@@ -18,4 +18,3 @@ resource "aws_eks_access_policy_association" "github_admin" {
 
 
 
-*/
